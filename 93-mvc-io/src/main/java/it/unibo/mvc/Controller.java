2806963ce1e128f14010a6af22c09b066db8cc69
MonkeSam <samuele.t.030204@gmail.com>
2024-11-25 18:42:05 +0100

    TODO: implementation of SimpleController; Controller interface DONE

--- 93-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 93-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,8 +1,38 @@
 package it.unibo.mvc;
 
+import java.util.List;
+
 /**
  *
  */
 public interface Controller {
+    /**
+     * Set the next string to print.
+     * 
+     * @param next
+     * 
+     * @throws NullPointerException Passing a null value is not accepted
+     */
+    public void setNext(final String next);
+
+    /**
+     * 
+     * @return next string to print
+     */
+    public String getNext();
+
+    /**
+     * 
+     * @return history of printed strings
+     * 
+     */
+    public List<String> getHistory();
+
+    /**
+     * Prints the current string.
+     * 
+     * @throws IllegalStateException
+     */
+    public void printCurrent();
 
 }
--- 93-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
+++ 93-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
@@ -1,9 +1,35 @@
 package it.unibo.mvc;
 
+import java.util.List;
+
 /**
  * 
  *
  */
 public final class SimpleController implements Controller {
+
+    @Override
+    public void setNext(String next) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setNext'");
+    }
+
+    @Override
+    public String getNext() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getNext'");
+    }
+
+    @Override
+    public List<String> getHistory() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getHistory'");
+    }
+
+    @Override
+    public void printCurrent() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'printCurrent'");
+    }
 
 }
